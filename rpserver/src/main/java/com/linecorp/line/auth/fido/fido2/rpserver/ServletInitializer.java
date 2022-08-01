c8b60c6a7a69fb36541fa74a69cc6d3d295d5e41
kihwankim <clack2933@gmail.com>
2022-08-02 00:08:01 +0900

    chore: war로 설정 변경

---
+++ rpserver/src/main/java/com/linecorp/line/auth/fido/fido2/rpserver/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.linecorp.line.auth.fido.fido2.rpserver;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RpserverApplication.class);
+    }
+
+}
